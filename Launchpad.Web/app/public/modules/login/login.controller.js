20ff7bdd8baf94484a2ce52914be44c8be632924
Dianne Saba <dnsaba19@gmail.com>
2018-01-03 18:48:01 -0800

    Register functionality finished, starting login

---
+++ Launchpad.Web/app/public/modules/login/login.controller.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    "use strict";
+    angular.module("publicApp")
+        .component("login", {
+            templateUrl: "/app/public/modules/login/login-comp.html",
+            controller: "loginController"
+        });
+})();
+
+(function () {
+    "use strict";
+    angular.module("publicApp")
+        .controller("loginController", LoginController);
+
+    LoginController.$inject = ["$scope", "loginService"];
+
+    function LoginController($scope, loginService) {
+        var vm = this;
+        vm.$scope = $scope;
+        vm.loginService = loginService;
+        vm.item = {};
+
+        vm.$onInit = _onInit;
+        vm.login = _login;
+
+        function _onInit() {
+            console.log("login controller onInit");
+        }
+
+        function _login() {
+            console.log(vm.item);
+        }
+    }
+})();
---
+++ Launchpad.Web/app/public/modules/login/login.service.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+    angular.module("publicApp")
+        .factory("loginService", LoginService);
+
+    LoginService.$inject = ["$http", "$q"];
+
+    function LoginService($http, $q) {
+        return {
+            post: _post
+        };
+
+        function _post(data) {
+            return $http.post("/api/login",
+                data, { withCredentials: true })
+                .then(success).catch(error);
+        };
+
+        function success(res) {
+            return res;
+        }
+
+        function error(err) {
+            return $q.reject(err);
+        }
+    }
+})();
