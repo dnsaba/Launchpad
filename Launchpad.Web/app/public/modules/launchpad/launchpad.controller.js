b26f4460da49f9d0acfdf8034346b28114eed5b0
Dianne Saba <dnsaba19@gmail.com>
2018-01-07 22:07:26 -0800

    Changed some html, need to go back and put styling in separate file. Sounds play on keypress now, not onclick

--- Launchpad.Web/app/public/modules/launchpad/launchpad.controller.js
+++ Launchpad.Web/app/public/modules/launchpad/launchpad.controller.js
@@ -23,9 +23,13 @@
         vm.$onInit = _onInit;
         vm.testclick = _testclick;
         vm.testclick2 = _testclick2;
+        vm.detect = _detect;
+        vm.getSound = _getSound;
+        vm.playSound = _playSound;
 
         function _onInit() {
             console.log("launchpad controller onInit");
+            vm.detect();
         }
 
         function _testclick() {
@@ -38,6 +42,103 @@
             var audioclip = new Audio();
             audioclip.src = '/app/public/audio/lp_110_am_c_guitar_chord_delay.wav';
             audioclip.play();
+        }
+
+        function _detect() {
+            $('#keyboard').bind("keydown", function (e) {
+                console.log(e.keyCode);
+                var sound = vm.getSound(e.keyCode);
+                vm.playSound(sound);
+            })
+        }
+
+        function _playSound(sound) {
+            var audioclip = new Audio();
+            audioclip.src = sound;
+            audioclip.play();
+        }
+
+        function _getSound(kc) {
+            switch (kc) {
+                case 65:
+                    return "/app/public/audio/sound2.wav";
+                    break;
+                case 66:
+                    return "";
+                    break;
+                case 67:
+                    return "";
+                    break;
+                case 68:
+                    return "";
+                    break;
+                case 69:
+                    return "";
+                    break;
+                case 70:
+                    return "";
+                    break;
+                case 71:
+                    return "";
+                    break;
+                case 72:
+                    return "";
+                    break;
+                case 73:
+                    return "";
+                    break;
+                case 74:
+                    return "";
+                    break;
+                case 75:
+                    return "";
+                    break;
+                case 76:
+                    return "";
+                    break;
+                case 77:
+                    return "";
+                    break;
+                case 78:
+                    return "";
+                    break;
+                case 79:
+                    return "";
+                    break;
+                case 80:
+                    return "";
+                    break;
+                case 81:
+                    return "/app/public/audio/sample1.wav";
+                    break;
+                case 82:
+                    return "";
+                    break;
+                case 83:
+                    return "/app/public/audio/sound1.wav";
+                    break;
+                case 84:
+                    return "";
+                    break;
+                case 85:
+                    return "";
+                    break;
+                case 86:
+                    return "";
+                    break;
+                case 87:
+                    return "/app/public/audio/sample2.wav";
+                    break;
+                case 88:
+                    return "";
+                    break;
+                case 89:
+                    return "";
+                    break;
+                case 90:
+                    return "/app/public/audio/sample3.wav";
+                    break;
+            }
         }
     }
 })();
